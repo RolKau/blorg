ec4f2097d441fa5be05bccf0f99bec46981f3ed8
Roland Kaufmann <rlndkfmn P orgmode A gmail D com>
2011-07-05 10:36:25 +0200

    Custom glue to connect the WordPress theme to our template

---
+++ themes/inove/js/init.js
@@ -0,0 +1,4 @@
+function body_Load () {
+	searchbox_Load ();
+	document.getElementById('themeinfo').innerHTML = '<a href="http://wordpress.org/extend/themes/inove">iNove</a> theme by <a href="http://www.neoease.com">NeoEase</a>.';
+}
