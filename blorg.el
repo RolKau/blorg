cca5fd235a32328d2b5b6346915e2390f698d2ff
Roland Kaufmann <rlndkfmn P orgmode A gmail D com>
2012-05-08 21:53:53 +0200

    Updated version number

--- blorg.el
+++ blorg.el
@@ -1,6 +1,6 @@
 ;;; blorg.el --- export a blog from an org file
 
-(defconst blorg-version "0.75e-rk2" "`blorg' version.")
+(defconst blorg-version "0.75e-rk3" "`blorg' version.")
 
 ;; Copyright (c) 2006 Bastien Guerry
 ;; Portions Copyright (c) 2011 Roland Kaufmann
